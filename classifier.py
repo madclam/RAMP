da6f28af55e78d9012d3d7a4d8cdb80e7781d3da
Madclam <madclam@gmail.com>
2015-04-10 14:07:01 +0200

    troisieme essai

--- classifier.py
+++ classifier.py
@@ -1,4 +1,4 @@
-from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.preprocessing import Imputer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator
@@ -7,10 +7,12 @@ class Classifier(BaseEstimator):
     def __init__(self):
         self.clf = Pipeline([
             ('imputer', Imputer(strategy='most_frequent')),
-            ('rf', AdaBoostClassifier(
-                base_estimator=RandomForestClassifier(max_depth=5, n_estimators=10),
-                n_estimators=10)
+            ('rf', GradientBoostingClassifier(loss='deviance', learning_rate=0.1, n_estimators=100, subsample=1.0, min_samples_split=2, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_depth=3, init=None, random_state=None, max_features=None, verbose=0, max_leaf_nodes=None, warm_start=False)
             )
+#            ('rf', AdaBoostClassifier(
+#                base_estimator=RandomForestClassifier(max_depth=5, n_estimators=10),
+#                n_estimators=10)
+#            )
         ])
  
     def fit(self, X, y):
